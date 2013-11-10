1d40f2ca8dc26799a7fe11a2251104d8f5183dfd
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-11-10 06:39:26 +0000

    Chapter 11.Task F: Add a Dash of Ajax

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,12 +13,14 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
     current_item = line_items.find_by(product_id: product_id)
     if current_item
+      # This will not be reflected in Rails console because it will be on Database
       current_item.quantity += 1
     else
       current_item = line_items.build(product_id: product_id, 
                                       product_price: Product.find(product_id).price)
                                       #product_price: current_item.product.price)
       # Cant't use the second method because current_item is nil now
+      # This will be reflected in Rails console because 'build' is on memory
     end
     current_item
   end
