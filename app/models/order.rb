d8897d7ede49c7b12f4cdd80ede37e1146ac6f43
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-11-14 15:43:27 +0000

    Add PaymentType model

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,7 +14,7 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  PAYMENT_TYPES = PaymentType.all.pluck(:payment_type)
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
